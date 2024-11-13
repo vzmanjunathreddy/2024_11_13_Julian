b06fd9db17f8ba4c512e202cc88d37e4808cddc8
Julián Segura <jsegura@ri.gob.do>
2024-11-13 07:55:37 -0400

    question 4

---
+++ Qeustion4/FetDataAsync.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qeustion4;
+
+public class FetDataAsync
+{
+    public async Task<string> FetchDataFromAPI1()
+    {
+        return "data from API 1";
+    }
+
+    public async Task<string> FetchDataFromAPI2()
+    {
+        return "data from API 2";
+    }
+}
---
+++ Qeustion4/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Qeustion4;
+
+Console.WriteLine("Hello, World!");
+
+var fetchData = new FetDataAsync();
+
+var data1 = await fetchData.FetchDataFromAPI1();
+Console.WriteLine(data1);
+var data2 = await fetchData.FetchDataFromAPI2();
+Console.WriteLine(data2);
+
+var data3 = fetchData.FetchDataFromAPI1();
+var data4 = fetchData.FetchDataFromAPI2();
+
+
+var resultAll = await Task.WhenAll(data3, data4);
+Console.WriteLine(resultAll);
+
+var resultAny = await Task.WhenAny(data3,data4);
+Console.WriteLine(resultAny);
