ea9b35e4a51ff55bfa87e1ff48f393cfbedc1d90
Julián Segura <jsegura@ri.gob.do>
2024-11-13 07:35:02 -0400

    question 2

---
+++ Question2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var stack  = new Question2.Stack<int>();
+
+//verify list is empty
+Console.WriteLine(stack.IsEmpty());
+
+//should throw exception
+//stack.Pop();
+
+//stack.Peek();
+
+stack.Push(1);
+stack.Push(5);
+stack.Push(6);
+
+Console.WriteLine(stack.IsEmpty());
+Console.WriteLine(stack.Count());
+
+//check last element
+Console.WriteLine(stack.Peek());
+
+//remove 6 from stack
+stack.Pop();
---
+++ Question2/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2;
+
+
+//Push, Pop ,Peek, Count, IsEmpty
+public class Stack<T>
+{
+    private List<T> _lst;
+
+    public Stack()
+    {
+        _lst = new List<T>();
+    }
+
+    public void Push(T item)
+    {
+        _lst.Add(item);
+    }
+
+    public T Peek()
+    {
+        if (IsEmpty()) throw new InvalidOperationException("Stack is empty");
+
+        return _lst[_lst.Count - 1];
+    }
+
+    public void Pop()
+    {
+        if (IsEmpty()) throw new InvalidOperationException("Stack is empty");
+
+        var elm = _lst[_lst.Count - 1];
+        _lst.Remove(elm);
+    }
+
+    public int Count()
+    {
+        return _lst.Count;
+    }
+
+    public bool IsEmpty()
+    {
+        return _lst.Count == 0;
+    }
+}
